4815c367aeabb14b8456e2afdbaf8fc50e43cf28
mithumarymanoj <mithumarymanoj@gmail.com>
2021-09-16 16:09:28 +0530

    Added the new Poll SCM message

--- LtiBank/src/main/java/com/lti/App.java
+++ LtiBank/src/main/java/com/lti/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println(" Hi Mithu!!");
+        System.out.println("Hi I was built using Poll SCM");
     }
 }
